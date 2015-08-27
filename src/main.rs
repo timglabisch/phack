9af7bf7d8a91b009c458f30306ddf4cf2d1dc6bb
Tim Glabisch <tim@ainfach.de>
2015-08-27 15:10:17 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,37 @@ use easy_resolver::*;
 
 fn main() {
 
+    // registry beinhaltet eine liste aller Packages und dessen Versionen.
+    // Möchte man diese Liste aufbauen, so muss man nach und nach alle Package-Informationen von Packagist runterladen
+    // siehe: https://packagist.org/p/symfony/symfony.json
+    // und die entsprechenden Versionen eintragen.
+    // Die Registry beinhaltet alle verfügbaren Pakete (welche benötigt werden könnten).
     let mut registry = vec!(
+        pkg!("symfony", "2.7.0"),
+        pkg!("symfony", "2.7.1"),
+        // ...
         pkg!("foo", "1.0.0"),
         pkg!("foo", "2.0.0"),
         pkg!("foo", "0.1.0"),
         pkg!("foo", "0.2.0"),
         pkg!("food", "0.2.0"),
+        pkg!("symfony/finder", "2.7.0"),
+        
+        // der Paket Syntax erlaubt das definieren von Abhängigkeiten.
+        // jede Abhängigkeit (dep(endency)) ist ein Paket, welches in der Registry vorliegen muss.
+        pkg!("symfony", "2.7.3" => [dep("symfony/finder", "2.7.0")]),
         pkg!("d1", "0.4.2" => [dep("foo", "1"), dep("food", "0.2.*")]),
         pkg!("d2", "0.4.2" => [dep("foo", "2")]),
         pkg!("d3", "0.4.2" => [dep("foo", "0.1")]),
         pkg!("d4", "0.4.2" => [dep("foo", "0.2")]),
 
-
-        pkg!("base_project", "1.0.0" => [dep("d1", "*")]),
+        // Das eigentliche Projekt (eigener Paketname) muss auch in die Registry gelegt werden,
+        // sowie die Abhängigkeiten. Die Abhängigkeiten hier, ist quasi die Liste aus einer composer.lock.
+        pkg!("base_project", "1.0.0" => [dep("symfony", "*")]),
     );
 
+    // nun wollen wir wissen, welche Pakete unser Projekt benötigt,
+    / wenn alles gut geht, dann wird uns das Programm mitteilen, dass wir symfony und symfony/finder benötigen.
     let packages = vec![
         dep("base_project", "*"),
     ];
@@ -30,6 +46,8 @@ fn main() {
     let res = resolve(packages, &mut registry).unwrap();
 
     for package in res {
-        println!("{} {}", package.name(), package.version());
+        // hier könnte man via git die pakete einfach runterladen,
+        // der resolver hat rausgefunden welche pakete und vorallem welche versionen benötigt werden.
+        println!("{} {}", package.name(), package.version()); // symfony, symfony-finder
     }
 }
